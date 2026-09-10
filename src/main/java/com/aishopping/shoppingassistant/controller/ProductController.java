ce2c3f4a97b5aaa70f636c5d27c6ff08d0a70849
Hyndhavi <hyndaviyedida@gmail.com>
2026-09-10 22:38:12 +0530

    Add product REST controller

---
+++ src/main/java/com/aishopping/shoppingassistant/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.aishopping.shoppingassistant.controller;
+
+import com.aishopping.shoppingassistant.model.Product;
+import com.aishopping.shoppingassistant.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+}
